a8657c7b51bd6e7cd2838a7bad95c11c535bebf4
Josh <joshua.kilazoglou@gmail.com>
2015-05-13 12:12:15 +1000

    Just adding the AI files

---
+++ basicAI.java
@@ -0,0 +1,20 @@
+
+public class basicAI implements AI{
+
+	public int decideMove(Board currentBoard) {
+		
+		int move;
+		
+		
+		
+		return move;
+	}
+	
+	private int score(int move) {
+		//TODO
+		
+	}
+
+	
+	
+}
