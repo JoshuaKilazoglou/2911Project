beeab979be2719315456a5791ad7ada5540f11fa
Alan YI <245655664@qq.com>
2015-05-15 01:37:56 +1000

    Update again
    Maybe pushed in wrong files

--- UI.java
+++ UI.java
@@ -5,9 +5,10 @@ import javax.swing.*;
 public class UI extends JFrame {
 	private Game game;
 	private Layer chessPiecePanel;
+
 	/*
-	 * Precondition:Nothing
-	 * Postcondition:A frame containing new content is inserted to frame
+	 * Precondition:Nothing Postcondition:A frame containing new content is
+	 * inserted to frame
 	 */
 	public UI() {
 		super();
@@ -15,10 +16,10 @@ public class UI extends JFrame {
 	}
 
 	/**
-	 * Preconditon:Nothing
-	 * Postconditon:Meant to be invoke every time when it needs to restart or start the game 
+	 * Preconditon:Nothing Postconditon:Meant to be invoke every time when it
+	 * needs to restart or start the game
 	 */
-	public void creatConetent(){
+	public void creatConetent() {
 		game = new Game();
 		chessPiecePanel = new Layer();
 		frameSetup();
@@ -26,38 +27,35 @@ public class UI extends JFrame {
 		addingMouseAdapter();
 		setVisible(true);
 	}
-	
 
 	private void addingPanel() {
 		JLayeredPane layeredPane = getLayeredPane();
 		layeredPane.setVisible(true);
 		layeredPane.setSize(640, 480);
-		
+
 		JLabel board = new JLabel();
-		board.setBounds(0, 0,640,480);
+		board.setBounds(0, 0, 640, 480);
 		board.setVisible(true);
 		ImageIcon i = new ImageIcon(getClass().getResource("Connect4Board.png"));
 		board.setIcon(i);
 		board.setOpaque(true);
-		
+
 		layeredPane.add(board, new Integer(10));
 	}
 
-	private void addingMouseAdapter(){
+	private void addingMouseAdapter() {
 		JLayeredPane layeredPane = getLayeredPane();
-		
-		chessPiecePanel.setBounds(0, 0,640,480);
+
+		chessPiecePanel.setBounds(0, 0, 640, 480);
 		chessPiecePanel.setVisible(true);
 		chessPiecePanel.setOpaque(false);
-		
-		Mouse mouse = new Mouse(chessPiecePanel,game,this);
+
+		Mouse mouse = new Mouse(chessPiecePanel, game, this);
 		chessPiecePanel.addMouseListener(mouse);
-		
-		layeredPane.add(chessPiecePanel,new Integer(8));
+
+		layeredPane.add(chessPiecePanel, new Integer(8));
 	}
 
-
-	
 	/**
 	 * This is only called when we get into the whole gaming system
 	 */
