a862be3b41effe0bc466ecde2172f782d0d2eb62
defuyun <carlliu2012@gmail.com>
2015-05-16 20:49:13 +1000

    put BoadUI into UI

--- UI.java
+++ UI.java
@@ -2,14 +2,115 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Board extends JPanel implements MouseListener{
+	Game game;
+	Image img;
+	boolean isGameOver = false;
+
+	final static int ROW = 6;
+	final static int COL = 7;
+
+	public void restartGame(){
+		game.restartGame();
+		repaint();
+	}
+
+	public void undo(){
+		if(game.undo())
+			repaint();
+	}
+
+	public void redo(){
+		if(game.redo())
+			repaint();
+	}
+
+	public Board(){
+		game = new Game();
+		setBackground(Color.white);
+		img = Toolkit.getDefaultToolkit().getImage("Connect4Board.png");
+		addMouseListener(this);
+	}
+
+	public int getDisImgToBorder(){
+		int imgWidth = img.getWidth(this);
+		int WinWidth = getWidth();
+		return (WinWidth-imgWidth)/2;
+	}
+
+	public int getDisImgToTop(){
+		int imgHeight = img.getHeight(this);
+		int WinHeight = getHeight();
+		return (WinHeight-imgHeight)/2;
+	}
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		int x = getDisImgToBorder(); // to keep the picture in the middle
+		int y = getDisImgToTop();
+		g.drawImage(img,x,y,null);
+
+		for(int i = ROW-1; i >= 0; i--)
+			for(int j = 0; j < COL; j++){
+				g.setColor(Color.BLACK);
+				int col = Game.getX(j);
+				int row = Game.getY(ROW-1-i);
+				g.drawOval(x+col,y+row,Connect4Board.CRADUIS*2,Connect4Board.CRADUIS*2);
+				if(game.whatsHere(i,j) != 0){
+					g.setColor((game.whatsHere(i,j) == 1)? Color.RED:Color.YELLOW);
+					g.fillOval(x+col,y+row,Connect4Board.CRADUIS*2,Connect4Board.CRADUIS*2);
+				}
+				
+			}
+	}
+
+	public void mousePressed(MouseEvent e) {
+    	//
+    }  
+   
+   	public void mouseClicked(MouseEvent e) {  
+   		double location = e.getPoint().getX();
+   		int borderDis = getDisImgToBorder();
+   		int col = Game.getCol(borderDis,location);
+   		
+   		if(col == -1)
+   			return;
+
+   		if(!game.checkValidMove(col))
+   			return;
+
+   		int row = game.makeMove(col);
+   		/*
+			What happens when won? game.getState() = 2;
+			what happens when the board is full? game.getState() = 1;
+   		*/
+   		repaint();
+   	}  
+     
+   	public void mouseEntered(MouseEvent e) {  
+   		//
+   	}  
+   	
+   	public void mouseExited(MouseEvent e) {  
+   		//
+   	}  
+   	public void mouseReleased(MouseEvent e) {  
+   		//
+   	}
+}
+
 public class UI extends JFrame{
 	private JPanel toolbar;
 	private JButton startButton,undoButton,redoButton,exitButton,backToMenu;
-	private BoardGUI board;
+	private Board board;
 
 	public UI(){
 		super("UI");
-		board = new BoardGUI();
+		board = new Board();
 		add(board);
 		addToolBar();
 		board.setOpaque(true);
