bfb6f1254123132894cf73070a9b85ed4877a06b
z5018746 <z5018746@zmail.unsw.edu.au>
2015-05-22 14:22:12 +1000

    Update basicAI.java

--- basicAI.java
+++ basicAI.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class basicAI implements AI{
 
-	public int decideMove(Game currentBoard) {
-		
-		int move = 0;
-		
-
-		return move;
+	public int decideMove(Game currentBoard) {	
+		Random generator = new Random();
+		ArrayList<Integer> moves = new ArrayList<Integer>();
+		for (int i = 0; i < 7; i++) {
+			if (currentBoard.checkValidMove(i))
+				moves.add(i);
+		}
+		int rand = generator.nextInt(moves.size());
+		return moves.get(rand);
 	}
 	
 	private int score(int move) {
